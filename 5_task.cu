421b04c0f69d9d27b7abece5f3a0851e30d1bde1
Sovego <egor.sofronov03@gmail.com>
2023-05-19 05:22:59 +0000

    refactoring

--- 5_task.cu
+++ 5_task.cu
@@ -1,20 +1,17 @@
 
 #include <mpi.h>
-
-#include <math.h>
 #include <iostream>
+#include <sstream>
+#include <cstring>
 #include "cuda_runtime.h"
 #include <cub/cub.cuh>
 #include <cub/block/block_reduce.cuh>
-
 #ifdef NVPROF_
 #include </opt/nvidia/hpc_sdk/Linux_x86_64/22.11/cuda/11.8/targets/x86_64-linux/include/nvtx3/nvToolsExt.h>
 #endif
 
-#define at(arr, x, y) (arr[(x) * (n) + (y)])
-/*
-* Режимы распределения потоков по блокам
-*/
+
+
 constexpr int MAXIMUM_THREADS_PER_BLOCK = 32;
 #define THREAD_PER_BLOCK_DEFINED(arr_lenght_dim_1, arr_lenght_dim_2, max_thread) ((arr_lenght_dim_1+max_thread-1)/max_thread), ((arr_lenght_dim_2+max_thread-1)/max_thread)
 #define BLOCK_COUNT_DEFINED(arr_lenght_dim_1, arr_lenght_dim_2, threads_count) ((arr_lenght_dim_1 + threads_count.x - 1) / threads_count.x), ((arr_lenght_dim_2 + threads_count.y - 1) / threads_count.y)
@@ -31,15 +28,64 @@ constexpr int THREADS_PER_BLOCK_REDUCE = 256;
 
 constexpr int ITERS_BETWEEN_UPDATE = 400;
 
-void transfer_data(const int rank, const int ranks_count, double* F_from, double* F_to, int m,int n, cudaStream_t stream = 0){
+typedef struct cmdArgs{
+    bool showResultArr;
+    bool initUsingMean;
+    double eps;
+    int iterations;
+    int n; // Count of rows
+    int m; // Count of collumns
+} cmdArgs;
+
+template<typename T>
+T extractArgument(char* arr){
+    std::stringstream stream;
+    stream << arr;
+    T result;
+    if (!(stream >> result)){
+        throw std::invalid_argument("Wrong argument type");
+    }
+    return result;
+}
+
+void processArgs(int argc, char *argv[], cmdArgs* args){
+    for(int arg = 0; arg < argc; arg++){
+        if(std::strcmp(argv[arg], "-eps") == 0){
+            args->eps = extractArgument<double>(argv[arg+1]);
+            arg++;
+        }
+        else if(std::strcmp(argv[arg], "-i") == 0){
+            args->iterations = extractArgument<int>(argv[arg+1]);
+            arg++;
+        }
+        else if(std::strcmp(argv[arg], "-s") == 0){
+            args->n = extractArgument<int>(argv[arg+1]);
+            args->m = args->n;
+            arg++;
+        }
+    }
+}
+
+
+void printSettings(cmdArgs* args){
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "\tAccur: " << args->eps << std::endl;
+    std::cout << "\tMax iteration: " << (double)(args->iterations) << std::endl;
+    std::cout << "\tSize: " << args->n << 'x' << args->m << std::endl;
+}
+
+
+
+#define at(arr, x, y) (arr[(x) * (n) + (y)])
+void transfer_data(const int rank, const int ranks_count, double* F_from, double* F_to, cmdArgs& local_args, cudaStream_t stream = 0){
 
     if(rank != 0){
-        cudaMemcpyAsync(F_from + m, F_to + m, m * sizeof(double), cudaMemcpyDeviceToHost, stream);
+        cudaMemcpyAsync(F_from + local_args.m, F_to + local_args.m, local_args.m * sizeof(double), cudaMemcpyDeviceToHost, stream);
         MPI_Request rq;
         // отправляем указатель на вторую строку процессу, работующему сверху
         MPI_Isend(
-            F_from + m,
-            m,
+            F_from + local_args.m,
+            local_args.m,
             MPI_DOUBLE,
             rank-1,
             rank-1,
@@ -51,10 +97,10 @@ void transfer_data(const int rank, const int ranks_count, double* F_from, double
     // отправляем свою вторую строку вниз
     if(rank != ranks_count-1){
         MPI_Request rq;
-        cudaMemcpyAsync(F_from + m*(n-2), F_to + m*(n-2), m * sizeof(double), cudaMemcpyDeviceToHost, stream);
+        cudaMemcpyAsync(F_from + local_args.m*(local_args.n-2), F_to + local_args.m*(local_args.n-2), local_args.m * sizeof(double), cudaMemcpyDeviceToHost, stream);
         MPI_Isend(
-            F_from + m*(n-2),
-            m,
+            F_from + local_args.m*(local_args.n-2),
+            local_args.m,
             MPI_DOUBLE,
             rank+1,
             rank+1,
@@ -66,29 +112,25 @@ void transfer_data(const int rank, const int ranks_count, double* F_from, double
     // принимаем строку от верхнего
     if(rank != 0){
         MPI_Status status;
-        MPI_Recv(F_from, m, MPI_DOUBLE, rank-1, rank, MPI_COMM_WORLD, &status);
-        cudaMemcpyAsync(F_to, F_from, m * sizeof(double), cudaMemcpyHostToDevice, stream);
+        MPI_Recv(F_from, local_args.m, MPI_DOUBLE, rank-1, rank, MPI_COMM_WORLD, &status);
+        cudaMemcpyAsync(F_to, F_from, local_args.m * sizeof(double), cudaMemcpyHostToDevice, stream);
 
     }
     // принимаем строку от нижнего
     if(rank != ranks_count - 1){
         MPI_Status status;
-        MPI_Recv(F_from+(m * (n-1)), m, MPI_DOUBLE, rank+1, rank, MPI_COMM_WORLD, &status);
-        cudaMemcpyAsync(F_to+(m * (n-1)), F_from+(m * (n-1)), m * sizeof(double), cudaMemcpyHostToDevice, stream);
+        MPI_Recv(F_from+(local_args.m * (local_args.n-1)), local_args.m, MPI_DOUBLE, rank+1, rank, MPI_COMM_WORLD, &status);
+        cudaMemcpyAsync(F_to+(local_args.m * (local_args.n-1)), F_from+(local_args.m * (local_args.n-1)), local_args.m * sizeof(double), cudaMemcpyHostToDevice, stream);
     }
 }
-
-
-__global__ void iterate(const double *F, double *Fnew,const int* m){
-
+__global__ void iterate(double *F, double *Fnew, const cmdArgs *args){
 
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
+    if (j == 0 || i == 0 || i >= args->n - 1 || j >= args->m - 1) return; // Don't update borders
 
-    if (j == 0 || i == 0 || i >= *m - 1 || j >= *m - 1) return; // Don't update borders
-
-    int n = *m;
+    int n = args->m;
     at(Fnew, i, j) = 0.25 * (at(F, i + 1, j) + at(F, i - 1, j) + at(F, i, j + 1) + at(F, i, j - 1));
 }
 
@@ -111,12 +153,8 @@ __global__ void block_reduce(const double *in1, const double *in2, const int n, 
         out[blockIdx.x] = block_max_diff;
     }
 }
-
-
 int main(int argc, char *argv[]) {
 
-
-    
 // ------------------
 // Подготовка к работе
 // ------------------
@@ -133,25 +171,20 @@ int main(int argc, char *argv[]) {
     cudaSetDevice(rank);
 
     // ====== Парсинг аргументов командной строки ======
-    //cmdArgs global_args = cmdArgs{false, false, 1E-6, (int)1E6, 16, 16};
-    //processArgs(argc, argv, &global_args);
-    int n_g {std::stoi(argv[4])},m_g {std::stoi(argv[4])}; // n m - grid size
-    double error_g {1},tol_g {std::stod(argv[2])}; // error - error value. tol - accuracy
-    int iter_g {0},iter_max_g {std::stoi(argv[6])}; // iter - iterator. iter_max - max iteration count
-    int n_l,m_l;
+    cmdArgs global_args = cmdArgs{1E-6, (int)1E6, 16, 16};
+    processArgs(argc, argv, &global_args);
+
     if(rank == 0){
-        std::cout << "-----------------------------" << std::endl;
-    std::cout << "- Accuracy: " << tol_g << std::endl << "- Max iteration count: "<< iter_max_g << std::endl << "- Grid size: " << n_g << std::endl;
-    std::cout << "-----------------------------" << std::endl;
+        printSettings(&global_args);
     }
 
     // ====== Расчет элементов для каждого процесса ======
-    int TOTAL_GRID_SIZE = m_g * n_g;
+    int TOTAL_GRID_SIZE = global_args.m * global_args.n;
 
-    //cmdArgs local_args{global_args};
-    n_l =  TOTAL_GRID_SIZE / ranks_count / m_g + 2 * (rank != ranks_count - 1);
-    m_l=m_g;
-    int ELEMENTS_BY_PROCESS = n_l * m_l;
+    cmdArgs local_args{global_args};
+    local_args.n =  TOTAL_GRID_SIZE / ranks_count / global_args.m + 2 * (rank != ranks_count - 1);
+
+    int ELEMENTS_BY_PROCESS = local_args.n * local_args.m;
 
     // ====== Создание указателей на массивы ======
     double *F_H_full = nullptr; // Указатель для хранения всего массива (используется в rank = 0)
@@ -178,14 +211,19 @@ int main(int argc, char *argv[]) {
 // Кроме последнего, он обрабатывает только global_args.n / 4 строк
 // Это происходит из-за того, что ему нет необходимости поддерживать граничные значения с нижним блоком (он является самым нижним)
 // ------------------
-{ 
-    int n = n_g;
-    int m = m_g;
+{
+    int n = global_args.n;
+    int m = global_args.m;
 
-    // Заполняем полный массив в 0 процессе 
+    // Заполняем полный массив в 0 процессе
     if(rank == 0){
-        
+
         F_H_full = (double*)calloc(n*m, sizeof(double));
+
+        for (int i = 0; i < n * m && global_args.initUsingMean; i++){
+            F_H_full[i] = (LEFT_UP + LEFT_DOWN + RIGHT_UP + RIGHT_DOWN) / 4;
+        }
+
         at(F_H_full, 0, 0) = LEFT_UP;
         at(F_H_full, 0, m - 1) = RIGHT_UP;
         at(F_H_full, n - 1, 0) = LEFT_DOWN;
@@ -206,7 +244,7 @@ int main(int argc, char *argv[]) {
     // ------------------
         for(size_t target = 0; target < ranks_count; target++){
             MPI_Request req;
-            data_lenght = ELEMENTS_BY_PROCESS - 2 * m_l * (target == (ranks_count - 1) && ranks_count != 1);
+            data_lenght = ELEMENTS_BY_PROCESS - 2 * local_args.m * (target == (ranks_count - 1) && ranks_count != 1);
             MPI_Isend(
                 F_H_full + data_start,
                 data_lenght,
@@ -216,11 +254,11 @@ int main(int argc, char *argv[]) {
                 MPI_COMM_WORLD,
                 &req
             );
-            
-            data_start += data_lenght - m_l * 2;
-            
+
+            data_start += data_lenght - local_args.m * 2;
+
         }
-        
+
     }
 
 // ------------------
@@ -260,23 +298,10 @@ int main(int argc, char *argv[]) {
 // ------------------
 {
 
-    //cmdArgs *args_d;
-    //double* error_d;
-    int* iterations_d;
-    int* iter_max_d;
-    double* tol_d;
-    //cudaMalloc(&error_d, sizeof(double));
-    int * n_d;
-    int* m_d;
-    cudaMalloc(&n_d, sizeof(int));
-    cudaMalloc(&m_d, sizeof(int));
-    cudaMalloc(&iterations_d, sizeof(int));
-    cudaMalloc(&iter_max_d,sizeof(int));
-    cudaMalloc(&tol_d, sizeof(double));
-    cudaMemcpy(iter_max_d, &iter_max_g, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(tol_d, &tol_g, sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(n_d, &n_g, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(m_d, &m_g, sizeof(double), cudaMemcpyHostToDevice);
+    cmdArgs *args_d;
+    cudaMalloc(&args_d, sizeof(cmdArgs));
+    cudaMemcpy(args_d, &local_args, sizeof(cmdArgs), cudaMemcpyHostToDevice);
+
     int num_blocks_reduce = (ELEMENTS_BY_PROCESS + THREADS_PER_BLOCK_REDUCE - 1) / THREADS_PER_BLOCK_REDUCE;
 
     double *error_reduction;
@@ -292,33 +317,33 @@ int main(int argc, char *argv[]) {
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
-    dim3 threadPerBlock {THREAD_PER_BLOCK_DEFINED(n_l, m_l, MAXIMUM_THREADS_PER_BLOCK)}; // ПЕРЕОСМЫСЛИТЬ
+    dim3 threadPerBlock {THREAD_PER_BLOCK_DEFINED(local_args.n, local_args.m, MAXIMUM_THREADS_PER_BLOCK)}; // ПЕРЕОСМЫСЛИТЬ
     if(threadPerBlock.x > MAXIMUM_THREADS_PER_BLOCK){
         threadPerBlock.x = MAXIMUM_THREADS_PER_BLOCK;
     }
     if(threadPerBlock.y > MAXIMUM_THREADS_PER_BLOCK){
         threadPerBlock.y = MAXIMUM_THREADS_PER_BLOCK;
-    } 
-    dim3 blocksPerGrid {BLOCK_COUNT_DEFINED(n_l, m_l, threadPerBlock)}; // ПЕРЕОСМЫСЛИТЬ
+    }
+    dim3 blocksPerGrid {BLOCK_COUNT_DEFINED(local_args.n, local_args.m, threadPerBlock)}; // ПЕРЕОСМЫСЛИТЬ
 
 
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
 #ifdef NVPROF_
     nvtxRangePush("MainCycle");
 #endif
     do {
 
-        iterate<<<blocksPerGrid, threadPerBlock, 0, stream>>>(F_D, Fnew_D, m_d);
+        iterate<<<blocksPerGrid, threadPerBlock, 0, stream>>>(F_D, Fnew_D, args_d);
 
         // ОБМЕН ГРАНИЧНЫМИ УСЛОВИЯМИ
-        transfer_data(rank, ranks_count, F_H, F_D, m_l,n_l, stream);
+        transfer_data(rank, ranks_count, F_H, F_D, local_args, stream);
 
-        iterate<<<blocksPerGrid, threadPerBlock, 0, stream>>>(Fnew_D, F_D, m_d);
+        iterate<<<blocksPerGrid, threadPerBlock, 0, stream>>>(Fnew_D, F_D, args_d);
 
         // ОБМЕН ГРАНИЧНЫМИ УСЛОВИЯМИ
-        transfer_data(rank, ranks_count, F_H, Fnew_D, m_l,n_l, stream);
-        
+        transfer_data(rank, ranks_count, F_H, Fnew_D, local_args, stream);
+
         iterationsElapsed += 2;
         if(iterationsElapsed % ITERS_BETWEEN_UPDATE == 0){
             block_reduce<<<num_blocks_reduce, THREADS_PER_BLOCK_REDUCE, 0, stream>>>(F_D, Fnew_D, ELEMENTS_BY_PROCESS, error_reduction);
@@ -342,12 +367,45 @@ int main(int argc, char *argv[]) {
                 MPI_Bcast(&error, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
             }
         }
-    } while(error > tol_g && iterationsElapsed < iter_max_g);
+    } while(error > global_args.eps && iterationsElapsed < global_args.iterations);
 #ifdef NVPROF_
     nvtxRangePop();
 #endif
     cudaStreamDestroy(stream);
 }
+
+
+    if(global_args.showResultArr){
+// ------------------
+// Отправка финального массива на нулевой процесс
+// ------------------
+        cudaMemcpy(F_H, F_D, array_size_bytes, cudaMemcpyDeviceToHost);
+        MPI_Request req;
+        MPI_Isend(F_H + local_args.m, ELEMENTS_BY_PROCESS - (local_args.m * 2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &req);
+
+// ------------------
+// Отображение финального массива с нулевого процесса
+// ------------------
+        if(rank == 0){
+            int array_offset = local_args.m;
+            for(int target = 0; target < ranks_count; target++){
+                MPI_Status status;
+                int recive_size = ELEMENTS_BY_PROCESS - 2 * local_args.m - 2 * local_args.m * (target == (ranks_count - 1));
+                MPI_Recv(F_H_full + array_offset, recive_size, MPI_DOUBLE, target, 0, MPI_COMM_WORLD, &status);
+                array_offset += recive_size;
+            }
+
+            std::cout << rank << " ---\n";
+            for (int x = 0; x < global_args.n; x++) {
+                    int n = global_args.m;
+                    for (int y = 0; y < global_args.m; y++) {
+                        std::cout << at(F_H_full, x, y) << ' ';
+                    }
+                    std::cout << std::endl;
+                }
+            std::cout << std::endl;
+        }
+    }
 
     if(rank == 0){
         std::cout << "Iterations: " << iterationsElapsed << std::endl;
