4cc2fd85e6a3dcbf3c0117f9bb85eb2253328fac
Sovego <egor.sofronov03@gmail.com>
2023-05-23 14:14:00 +0000

    refactoring

--- 6_task.cu
+++ 6_task.cu
@@ -17,9 +17,6 @@
 #ifdef NVPROF_
 #include <nvToolsExt.h>
 #endif
-enum class PARAMETR_SOURCE{
-    FILE, GENERATE
-};
 
 enum class ARRAY_SOURCE{
     DEVICE, HOST
@@ -29,76 +26,76 @@ using calc_type = float;
 
 
 
-template<typename data_type>
+
 class MDT_array{
 public:
 
     MDT_array();
-    MDT_array(int, data_type*, ARRAY_SOURCE src_type = ARRAY_SOURCE::HOST);
+    MDT_array(int, float*, ARRAY_SOURCE src_type = ARRAY_SOURCE::HOST);
     MDT_array(int);
     ~MDT_array();
 
-    __device__ data_type& operator[] (int index); // Получение I-ого элемента на видеокарте
-    data_type operator() (int index); // Получение I-ого элемента и копирование его сразу на хост
-    data_type* operator&() const; // Отдает указатель на массив на видеокарта
-    int get_size();
+    __device__ float& operator[] (int index); // Получение I-ого элемента на видеокарте
+    float operator() (int index); // Получение I-ого элемента и копирование его сразу на хост
+    float* operator&() const; // Отдает указатель на массив на видеокарта
+    int get_size() const;
 private:
-    data_type* array_;
+    float* array_{};
     int size_;
 };
 
-template<typename data_type>
-MDT_array<data_type>::MDT_array(int size, data_type* src, ARRAY_SOURCE src_type) : size_(size){
-    cudaMalloc(&array_, size_*sizeof(data_type));
+
+MDT_array::MDT_array(int size, float* src, ARRAY_SOURCE src_type) : size_(size){
+    cudaMalloc(&array_, size_*sizeof(float));
     if(src_type == ARRAY_SOURCE::DEVICE){
-        cudaMemcpy(array_, src, sizeof(data_type) * size, cudaMemcpyDeviceToDevice);
+        cudaMemcpy(array_, src, sizeof(float) * size, cudaMemcpyDeviceToDevice);
     }
     else if(src_type == ARRAY_SOURCE::HOST){
-        cudaMemcpy(array_, src, sizeof(data_type) * size, cudaMemcpyHostToDevice);
+        cudaMemcpy(array_, src, sizeof(float) * size, cudaMemcpyHostToDevice);
     }
 }
 
-template<typename data_type>
-MDT_array<data_type>::MDT_array(){
+
+MDT_array::MDT_array(){
      throw std::runtime_error("You should specifed size");
 }
 
-template<typename data_type>
-MDT_array<data_type>::MDT_array(int size) : size_(size){
-    cudaMalloc(&array_, size_*sizeof(data_type));
+
+MDT_array::MDT_array(int size) : size_(size){
+    cudaMalloc(&array_, size_*sizeof(float));
 }
 
-template<typename data_type>
-MDT_array<data_type>::~MDT_array(){
+
+MDT_array::~MDT_array(){
     cudaFree(array_);
 }
 
-template<typename data_type>
-__device__ data_type& MDT_array<data_type>::operator[](int index){
+
+__device__ float& MDT_array::operator[](int index){
     return array_[index];
 }
 
-template<typename data_type>
-data_type* MDT_array<data_type>::operator&() const{
+
+float* MDT_array::operator&() const{
     return array_;
 }
 
-template<typename data_type>
-data_type MDT_array<data_type>::operator()(int index){
-    data_type tmp;
-    cudaMemcpy(&tmp, array_ + index, sizeof(data_type), cudaMemcpyDeviceToHost);
+
+float MDT_array::operator()(int index){
+    float tmp;
+    cudaMemcpy(&tmp, array_ + index, sizeof(float), cudaMemcpyDeviceToHost);
     return tmp;
 }
 
-template<typename data_type>
-int MDT_array<data_type>::get_size(){
+
+int MDT_array::get_size() const{
     return size_;
 }
 
 class Layer {
 public:
-    virtual MDT_array<calc_type>& forward(MDT_array<calc_type>& input) = 0;
-    MDT_array<calc_type>& operator()(MDT_array<calc_type>& input){
+    virtual MDT_array& forward(MDT_array& input) = 0;
+    MDT_array& operator()(MDT_array& input){
         return forward(input);
     }
 };
@@ -106,27 +103,27 @@ public:
 class FullyConnectedLayer : public Layer {
 public:
     FullyConnectedLayer();
-    FullyConnectedLayer(int input_size, int output_size, std::string weigth_path = "", std::string bias_path = "");
 
-    void set_weights(const PARAMETR_SOURCE source_type, const std::string file_path = "");
-    void set_bias(const PARAMETR_SOURCE source_type, const std::string file_path = "");
+    [[maybe_unused]] FullyConnectedLayer(int input_size, int output_size, const std::string& weigth_path = "", const std::string& bias_path = "");
+
+    void set_weights(const std::string& file_path = "");
+    void set_bias(const std::string& file_path = "");
 
     ~FullyConnectedLayer();
 
-    MDT_array<calc_type>& forward(MDT_array<calc_type>& input) override;
+    MDT_array& forward(MDT_array& x) override;
 
-    int get_size();
+    [[nodiscard]] int get_size() const;
     std::pair<int, int> get_dims();
 
 private:
-    void generate_parmeter(calc_type* target, const std::pair<int, int> size, const int random_seed = -1);
-    void load_parameter(calc_type* target, const std::string file_path, const std::pair<int, int> size);
+    static  void load_parameter(calc_type* target, const std::string& file_path, std::pair<int, int> size);
 
     int input_size_;
     int output_size_;
     float alpha_ = 1.0f;
     float beta_ = 0.0f;
-    MDT_array<calc_type>* d_output_;
+    MDT_array* d_output_;
     calc_type* d_weight_;
     calc_type* d_bias_;
     cublasHandle_t handle_;
@@ -135,10 +132,10 @@ private:
 using FC = FullyConnectedLayer;
 
 class Sigmoid : public Layer {
-    MDT_array<calc_type>& forward(MDT_array<calc_type>& input) override;
+    MDT_array& forward(MDT_array& arr) override;
 };
 
-MDT_array<calc_type>& Sigmoid_F(MDT_array<calc_type>& arr);
+MDT_array& Sigmoid_F(MDT_array& arr);
 
 class Sequential : public Layer {
 public:
@@ -147,7 +144,7 @@ public:
 
     void add_layer(std::unique_ptr<Layer> layer);
 
-    MDT_array<calc_type>& forward(MDT_array<calc_type>& input) override;
+    MDT_array& forward(MDT_array& x) override;
 
 private:
     std::vector<std::unique_ptr<Layer>> layers;
@@ -160,7 +157,7 @@ std::unique_ptr<T> create_layer(Args&&... args) {
     return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
-static void load_binary_file(calc_type* target, const std::string file_path, const std::pair<int, int> size){
+static void load_binary_file(calc_type* target, const std::string& file_path, const std::pair<int, int> size){
     std::ifstream file(file_path, std::ios::binary);
 
     calc_type* file_info;
@@ -177,17 +174,17 @@ static void load_binary_file(calc_type* target, const std::string file_path, con
     }
 
     cudaMemcpy(target, arr_host, size.first*size.second*sizeof(calc_type), cudaMemcpyHostToDevice);
-    
+
     cudaFreeHost(file_info);
     cudaFreeHost(arr_host);
 }
-Sequential::Sequential(){}
+Sequential::Sequential()= default;
 
 void Sequential::add_layer(std::unique_ptr<Layer> layer){
     layers.push_back(std::move(layer));
 }
 
-MDT_array<calc_type>& Sequential::forward(MDT_array<calc_type>& x){
+MDT_array& Sequential::forward(MDT_array& x){
     for(auto& lr : layers){
         x = lr->forward(x);
     }
@@ -199,12 +196,12 @@ __global__ void sigmoid_cuda(calc_type* arr, int n){
     }
 }
 
-MDT_array<calc_type>& Sigmoid::forward(MDT_array<calc_type>& arr) {
+MDT_array& Sigmoid::forward(MDT_array& arr) {
     sigmoid_cuda<<<1, arr.get_size()>>>(&arr, arr.get_size());
     return arr;
 }
 
-MDT_array<calc_type>& Sigmoid_F(MDT_array<calc_type>& arr){
+MDT_array& Sigmoid_F(MDT_array& arr){
     return Sigmoid()(arr);
 }
 
@@ -213,24 +210,14 @@ FullyConnectedLayer::FullyConnectedLayer(){
     throw std::runtime_error("You should specifed in_size and out_size");
 }
 
-FullyConnectedLayer::FullyConnectedLayer(int input_size, int output_size, std::string weigth_path, std::string bias_path) : input_size_(input_size), output_size_(output_size){
-    d_output_ = new MDT_array<calc_type>(output_size);
+[[maybe_unused]] FullyConnectedLayer::FullyConnectedLayer(int input_size, int output_size, const std::string& weigth_path, const std::string& bias_path) : input_size_(input_size), output_size_(output_size){
+    d_output_ = new MDT_array(output_size);
     cudaMalloc(&d_weight_, input_size_ * output_size_ * sizeof(calc_type));
     cudaMalloc(&d_bias_, output_size_ * sizeof(calc_type));
     cublasCreate(&handle_);
+    set_weights(weigth_path);
+    set_bias( bias_path);
 
-    if(weigth_path == ""){
-        set_weights(PARAMETR_SOURCE::GENERATE);
-    }
-    else{
-        set_weights(PARAMETR_SOURCE::FILE, weigth_path);
-    }
-    if(bias_path == ""){
-        set_bias(PARAMETR_SOURCE::GENERATE);
-    }
-    else{
-        set_bias(PARAMETR_SOURCE::FILE, bias_path);
-    }
 
 }
 
@@ -242,81 +229,46 @@ FullyConnectedLayer::~FullyConnectedLayer() {
     cudaFree(d_bias_);
 }
 
-MDT_array<calc_type>& FullyConnectedLayer::forward(MDT_array<calc_type>& x) {
+MDT_array& FullyConnectedLayer::forward(MDT_array& x) {
     calc_type* input = &x;
     cublasSgemv(handle_, CUBLAS_OP_T, input_size_, output_size_, &alpha_, d_weight_, input_size_, input, 1, &beta_, &*d_output_, 1);
     cublasSaxpy(handle_, output_size_, &alpha_, d_bias_, 1, &*d_output_, 1);
     return *d_output_;
 }
 
-int FullyConnectedLayer::get_size(){
+int FullyConnectedLayer::get_size() const{
     return output_size_ * input_size_;
 }
 std::pair<int, int> FullyConnectedLayer::get_dims(){
     return {input_size_, output_size_};
 }
 
-void FullyConnectedLayer::set_weights(const PARAMETR_SOURCE source_type, const std::string file_path){
-    if(source_type == PARAMETR_SOURCE::FILE){
+void FullyConnectedLayer::set_weights(const std::string& file_path){
+
         load_parameter(d_weight_, file_path, {input_size_, output_size_});
-    }
-    else if(source_type == PARAMETR_SOURCE::GENERATE){
-        generate_parmeter(d_weight_, {input_size_, output_size_});
-    }
+
 }
 
-void FullyConnectedLayer::set_bias(const PARAMETR_SOURCE source_type, const std::string file_path){
-    if(source_type == PARAMETR_SOURCE::FILE){
+void FullyConnectedLayer::set_bias(const std::string& file_path){
+
         load_parameter(d_bias_, file_path, {1, output_size_});
-    }
-    else if(source_type == PARAMETR_SOURCE::GENERATE){
-        generate_parmeter(d_bias_, {1, output_size_});
-    }
+
 }
 
-void FullyConnectedLayer::generate_parmeter(calc_type* target, const std::pair<int, int> size, const int random_seed){
-    int in_size = size.first;
-    int out_size = size.second;
-    
-    std::default_random_engine rangen;
-    if(random_seed != -1){
-        rangen.seed(random_seed);
-    }
-
-    calc_type disp = std::sqrt((double)2/out_size);
-    calc_type mean = 0;
-
-    std::normal_distribution<calc_type> distribution(mean, disp);
-
-    if(target == nullptr){
-        cudaMalloc(&target, in_size*out_size*sizeof(calc_type));
-    }
-
-    calc_type* temp = (calc_type*)malloc(in_size*out_size*sizeof(calc_type));
-
-    for(size_t i = 0; i < in_size*out_size; i++){
-        temp[i] = distribution(rangen);
-    }
-
-    cudaMemcpy(target, temp, in_size*out_size*sizeof(calc_type), cudaMemcpyHostToDevice);
-
-    free(temp);
-}
-
-void FullyConnectedLayer::load_parameter(calc_type* target, const std::string file_path, const std::pair<int, int> size){
+void FullyConnectedLayer::load_parameter(calc_type* target, const std::string& file_path, const std::pair<int, int> size){
     load_binary_file(target, file_path, size);
 }
 
 
 
 int main(){
-    
+
     Sequential Net;
 
-    MDT_array<calc_type> a {1024};
+    MDT_array a {1024};
 
     load_binary_file(&a, "torch/input", {1, 1024});
-    
+
     Net.add_layer(create_layer<FC>(1024, 256, "torch/fc1_weights", "torch/fc1_bias"));
     Net.add_layer(create_layer<Sigmoid>());
 
@@ -331,7 +283,7 @@ int main(){
 #ifdef NVPROF_
     nvtxRangePush("Forward_pass");
 #endif
-    MDT_array<calc_type> b = Net.forward(a);
+    MDT_array b = Net.forward(a);
 #ifdef NVPROF_
     nvtxRangePop();
 #endif
